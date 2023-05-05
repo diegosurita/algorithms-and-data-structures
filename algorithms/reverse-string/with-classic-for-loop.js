75e409888a8f9841124ad31a08fc240416b87a67
Diego Surita <suritadev@gmail.com>
2023-05-05 09:07:04 -0300

    remove param type validation on reverse with classic for loop exercise

--- algorithms/reverse-string/with-classic-for-loop.js
+++ algorithms/reverse-string/with-classic-for-loop.js
@@ -1,8 +1,4 @@
 function reverse(str) {
-    if (typeof str !== 'string') {
-        throw new Error('Param has to be an string');
-    }
-
     let result = '';
 
     for (let i = str.length -1; i >= 0; i--) {
